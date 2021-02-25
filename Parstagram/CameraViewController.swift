551b37b85aa91fe7de4e359029d4ce4d9869861b
Brayam Corral Flores <brayamcorral@gmail.com>
2021-02-24 21:51:13 -0800

    Image to post pops up on imageView

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import Alamo
+import AlamofireImage
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -35,6 +35,13 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to: size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
