b758f018fe92ba0d6a5ad7ba8f98dd9651b9defc
Brayam Corral Flores <brayamcorral@gmail.com>
2021-02-25 22:18:20 -0800

    user can see uploaded posts

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Parse
+import AlamofireImage
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
@@ -50,11 +51,13 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         cell.captionLabel.text = post["caption"] as! String
         
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af_setImage(withURL: url)
+        
         return cell
         
     }
-    
-
-    
-
 }
